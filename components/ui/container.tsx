59af76fef2eaf32b0bca8b7ea5db4a0692ae3a0f
f0reachARR <f0reach@f0reach.me>
2021-02-21 21:19:04 +0900

    Add section title and container

---
+++ components/ui/container.tsx
@@ -0,0 +1,3 @@
+export const AppContainer: React.FunctionComponent = ({ children }) => {
+  return <div className='container mx-auto my-2'>{children}</div>;
+};
---
+++ components/ui/section-title.tsx
@@ -0,0 +1,9 @@
+import clsx from 'clsx';
+
+export const SectionTitle: React.FunctionComponent = ({ children }) => {
+  return (
+    <h2 className={clsx('text-blue-light', 'border-b-4', 'text-xl')}>
+      {children}
+    </h2>
+  );
+};
