c0b3abaaf815bf247fcd2d841caff9c95bcd0c18
f0reachARR <f0reach@f0reach.me>
2021-02-23 15:22:18 +0900

    Add new club form

---
+++ pages/new/club.tsx
@@ -0,0 +1,79 @@
+import { ErrorMessage, Field, Form, Formik } from 'formik';
+import { useRouter } from 'next/router';
+import React from 'react';
+import { useCreateNewClubMutation, UserPermission } from '../../api/generated';
+import { Button } from '../../components/ui/button';
+import { AppContainer } from '../../components/ui/container';
+import { TextBox } from '../../components/ui/input';
+import { Loading } from '../../components/ui/loading';
+import { useAutoRedirect, useUser } from '../../utils/use-user';
+
+type FormProps = {
+  name: string;
+};
+const NewClub = () => {
+  const { mutateAsync, isLoading } = useCreateNewClubMutation();
+  const { isLoading: isUserLoading, isLoggedIn, user } = useUser();
+  const { push } = useRouter();
+
+  const handleSubmit = React.useCallback((values: FormProps) => {
+    mutateAsync({ name: values.name }).then(({ createNewClub }) => {
+      push('/club/[id]/edit', `/club/${createNewClub.id}/edit`);
+    });
+  }, []);
+
+  useAutoRedirect();
+
+  if (isUserLoading) {
+    return (
+      <AppContainer>
+        <Loading />
+      </AppContainer>
+    );
+  }
+
+  if (
+    !isLoggedIn ||
+    ![
+      UserPermission.Admin,
+      UserPermission.ClubMember,
+      UserPermission.Moderator,
+    ].includes(user!.permission)
+  ) {
+    return (
+      <AppContainer>
+        <p>Not allowed operation.</p>
+      </AppContainer>
+    );
+  }
+
+  return (
+    <AppContainer>
+      <Formik
+        initialValues={{
+          name: '',
+        }}
+        onSubmit={handleSubmit}
+      >
+        <Form>
+          <label htmlFor='title'>部活名</label>
+          <Field
+            as={TextBox}
+            name='title'
+            required
+            maxLength={32}
+            placeholder='Awesome Club activity'
+            disabled={isLoading}
+          ></Field>
+          <ErrorMessage name='title' />
+
+          <Button disabled={isLoading} type='submit'>
+            作成する
+          </Button>
+        </Form>
+      </Formik>
+    </AppContainer>
+  );
+};
+
+export default NewClub;
