3e93905c8302984c73633057c9c0201fdabb153f
f0reachARR <f0reach@f0reach.me>
2021-02-23 12:55:45 +0900

    Add variant for avatar

--- components/user/avatar.tsx
+++ components/user/avatar.tsx
@@ -1,13 +1,28 @@
+import clsx from 'clsx';
+
 type Props = {
   icon?: string;
+  variant?: 'large' | 'normal';
+  ringColor?: 'white' | 'blue';
 };
-export const Avatar = ({ icon }: Props) => {
+export const Avatar = ({
+  icon,
+  variant = 'normal',
+  ringColor = 'white',
+}: Props) => {
   return (
     <div>
       <img
         src={icon}
         alt='Avatar'
-        className='object-fill h-10 rounded-full ring-2 ring-white'
+        className={clsx('object-fill', 'rounded-full', {
+          'ring-2': variant === 'normal',
+          'ring-4': variant === 'large',
+          'h-10': variant === 'normal',
+          'h-32': variant === 'large',
+          'ring-white': ringColor === 'white',
+          'ring-blue-light': ringColor === 'blue',
+        })}
       />
     </div>
   );
