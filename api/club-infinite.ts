f48bb8a50b7cd632d32f963fc6428e5b119cecde
f0reachARR <f0reach@f0reach.me>
2021-03-04 13:05:46 +0900

    Add moderation waiting club list

--- api/club-infinite.ts
+++ api/club-infinite.ts
@@ -6,6 +6,9 @@ import {
   GetPublicClubsQueryVariables,
   GetPublicClubsDocument,
   ClubBasicFragment,
+  GetModerationWaitClubsQuery,
+  GetModerationWaitClubsQueryVariables,
+  GetModerationWaitClubsDocument,
 } from './generated';
 
 export const useGetPublicClubsInfiniteQuery = (limit = 30) => {
@@ -19,6 +22,39 @@ export const useGetPublicClubsInfiniteQuery = (limit = 30) => {
           offset: pageParam,
         },
       )();
+    },
+    {
+      getNextPageParam(lastPage, pages) {
+        return lastPage.getClubs.count < pages.length * limit
+          ? undefined
+          : pages.length * limit;
+      },
+    },
+  );
+  const count = React.useMemo(() => data?.pages[0].getClubs.count ?? 0, [data]);
+  const items = React.useMemo(
+    () =>
+      data?.pages.reduce<ClubBasicFragment[]>(
+        (arr, item) => [...arr, ...item.getClubs.items],
+        [],
+      ),
+    [data],
+  );
+
+  return { ...otherProps, count, items };
+};
+
+export const useGetModerationWaitClubsInfiniteQuery = (limit = 30) => {
+  const { data, ...otherProps } = useInfiniteQuery(
+    'getModerationWaitClubs',
+    ({ pageParam = 0 }) => {
+      return fetchGQL<
+        GetModerationWaitClubsQuery,
+        GetModerationWaitClubsQueryVariables
+      >(GetModerationWaitClubsDocument, {
+        limit,
+        offset: pageParam,
+      })();
     },
     {
       getNextPageParam(lastPage, pages) {
--- pages/me/moderation.tsx
+++ pages/me/moderation.tsx
@@ -1,5 +1,46 @@
+import React from 'react';
+import { useInView } from 'react-intersection-observer';
+import { useGetModerationWaitClubsInfiniteQuery } from '../../api/club-infinite';
+import { ClubListItem } from '../../components/club/list-item';
+import { AppContainer } from '../../components/ui/container';
+import { Loading } from '../../components/ui/loading';
+import { SectionTitle } from '../../components/ui/section-title';
+
 const ClubModerationPage = () => {
-  return <div></div>;
+  const {
+    count,
+    items,
+    hasNextPage,
+    isLoading,
+    isFetchingNextPage,
+    fetchNextPage,
+  } = useGetModerationWaitClubsInfiniteQuery(15);
+  const { ref, inView } = useInView();
+
+  React.useEffect(() => {
+    if (inView) {
+      fetchNextPage();
+    }
+  }, [inView]);
+
+  return (
+    <AppContainer>
+      <SectionTitle>承認待ち活動</SectionTitle>
+
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <div>
+          <p>{count}件</p>
+          {items!.map((item) => (
+            <ClubListItem key={item.id} club={item} />
+          ))}
+          {hasNextPage &&
+            (isFetchingNextPage ? <Loading /> : <div ref={ref} />)}
+        </div>
+      )}
+    </AppContainer>
+  );
 };
 
 export default ClubModerationPage;
