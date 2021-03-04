11cbaa6ffe052cd1efd4ccc9a8903e18543c45ed
f0reachARR <f0reach@f0reach.me>
2021-03-04 13:00:21 +0900

    Add link to moderation page

--- pages/me/index.tsx
+++ pages/me/index.tsx
@@ -1,12 +1,32 @@
+import Link from 'next/link';
 import React from 'react';
+import {
+  useGetModerationWaitCountQuery,
+  UserPermission,
+} from '../../api/generated';
 import { BelongClubList } from '../../components/club/belong-list';
 import { AppContainer } from '../../components/ui/container';
+import { ContentCard } from '../../components/ui/content-card';
 import { Loading } from '../../components/ui/loading';
 import { LargeUserInfo } from '../../components/user/user-info-lg';
-import { useAutoRedirect, useUser } from '../../utils/use-user';
+import {
+  useAutoRedirect,
+  usePermissionCheck,
+  useUser,
+} from '../../utils/use-user';
 
 const MypageIndex = () => {
   const { isLoading, isLoggedIn, user } = useUser();
+  const showModeration = usePermissionCheck([
+    UserPermission.Admin,
+    UserPermission.Moderator,
+  ]);
+  const { data: moderationInfo } = useGetModerationWaitCountQuery(
+    {},
+    {
+      enabled: showModeration,
+    },
+  );
 
   useAutoRedirect();
 
@@ -24,8 +44,25 @@ const MypageIndex = () => {
         <>
           <LargeUserInfo user={user!} />
           <div className='py-4'>
-            <h2 className='text-xl'>所属している部活動</h2>
-            <BelongClubList />
+            <section className='mb-2'>
+              <h2 className='text-xl'>所属している部活動</h2>
+              <BelongClubList />
+            </section>
+            {showModeration && (
+              <section className='mb-2'>
+                <h2 className='text-xl'>管理</h2>
+                <div className='grid grid-cols-cards col-auto gap-4'>
+                  <Link href='/me/moderation' passHref>
+                    <ContentCard
+                      title='承認待ちの活動'
+                      description={`${
+                        moderationInfo?.getClubs.count ?? 0
+                      }件の承認待ちがあります`}
+                    />
+                  </Link>
+                </div>
+              </section>
+            )}
           </div>
         </>
       )}
---
+++ pages/me/moderation.tsx
@@ -0,0 +1,5 @@
+const ClubModerationPage = () => {
+  return <div></div>;
+};
+
+export default ClubModerationPage;
