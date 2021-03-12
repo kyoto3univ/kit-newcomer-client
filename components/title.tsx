033892da8b67730bccda74e63afa6c2d5314134c
f0reachARR <f0reach@f0reach.me>
2021-03-12 11:09:34 +0900

    Add proper ogp

--- components/title.tsx
+++ components/title.tsx
@@ -7,6 +7,15 @@ type Props = {
 export const PageTitle = ({ title }: Props) => {
   return (
     <Head>
+      <meta property='og:type' content='website' />
+      <meta name='twitter:site' content='@f0reachKIT' />
+      <meta name='twitter:creator' content='@f0reachKIT' />
+      <meta name='twitter:title' content={title} />
+      <meta name='twitter:card' content='summary' />
+      <meta property='og:site_name' content='KIT新入生応援サイト' />
+      <meta property='og:locale' content='ja' />
+      <meta property='og:title' content={title} />
+      <link rel='shortcut icon' href='/icon.png' />
       <title>{title} | KIT新入生応援サイト</title>
     </Head>
   );
